f844951577278778e5edbb787d017d7b7ea2f909
Usuario <Usuario@CPD03-M03>
2024-04-15 12:21:55 -0300

    Adicionar arquivos de projeto.

---
+++ AppPassagem/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace AppPassagem
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
